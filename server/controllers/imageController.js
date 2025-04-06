c30c42dd2b95e2b0bab8feee99ee51a91eb37480
anurath <anurathwaghmode2018@gmail.com>
2025-04-06 23:05:26 +0530

    some controller files added

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,4 +1,6 @@
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
+import axios from "axios";
+import FormData from 'form-data';
 
 export const generateImage = async(req,res)=>{
     try{
@@ -16,12 +18,24 @@ export const generateImage = async(req,res)=>{
             return res.json({success:false,message:'No Credit Balance',creditBalance:user.creditBalance});
         }
 
+        const formData = new FormData()
+        formData.append('prompt',prompt)
 
+        const {data} = await axios.post('https://clipdrop-api.co/text-to-image/v1',formData,{
+            headers: {
+                ...formData.getHeaders(),
+                'x-api-key': process.env.CLIPDROP_API,
+              },
+              responseType:'arraybuffer'
+        })
 
-        
+        const base64Image = Buffer.from(data,'binary').toString('base64');
 
+        const resultImage = `data:image/png;base64,${base64Image}`;
 
+        await userModel.findByIdAndUpdate(user._id,{creditBalance :  user.creditBalance-1});
 
+        res.json({success:true,message:'Image Generated',creditBalance :  user.creditBalance-1,resultImage});
 
     }catch(error)
     {
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 
 import connectDB from './config/mongodb.js';
 import userRouter from './routes/userRoutes.js';
+import imageRouter from './routes/imageRoutes.js';
 
 const PORT = process.env.PORT||3000;
 const app = express();
@@ -12,7 +13,8 @@ app.use(cors());
 
 await connectDB();
 
-app.use('/api/user',userRouter)
+app.use('/api/user',userRouter);
+app.use('/api/image',imageRouter);
 
 app.get('/',(req,res)=>{
     res.send("Backend Started Om Numha Shivay!!!!!");
